5038b99ac8900678dda466d8333a80dad51c388f
FelipeRR42 <feliperojasretamales@gmail.com>
2026-05-25 21:18:12 -0400

    feat: final

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+
 #Iportar librerias
 import pandas
 import time
 import random
+import matplotlib.pyplot as plt
+
 start_time = time.time()
 print("Process inicio --- %s seconds ---" % (time.time() - start_time))
 
@@ -48,16 +51,25 @@ class ListaEnlazada:
 			aux_head = aux_head.sig
 		if aux_head != None:
 			return aux_head.dato
-
 		return aux_head
 
 	def recorrer(self):
 		aux_head = self.head
 		if aux_head is None:
 			return
-		while aux_head != None:
-			print(aux_head.dato, "\n------------------------------------------------------------------------------------------------------------------------")
-			aux_head = aux_head.sig
+		if type(aux_head.dato) is Usuario:
+			while aux_head != None:
+				print(f"Nombre Usuario: {aux_head.dato.username}\n-------------------------------------------------------------------------")
+				aux_head = aux_head.sig
+
+		elif type(aux_head.dato) is Post:
+			while aux_head != None:
+				print(f"Post ID: {aux_head.dato.post_id}, Owner: {aux_head.dato.owner}, Caption: {aux_head.dato.caption}, Likes: {aux_head.dato.cant_likes}\n-------------------------------------------------------------------------")
+				aux_head = aux_head.sig
+		else:
+			while aux_head != None:
+				print(f"{aux_head.dato}\n-------------------------------------------------------------------------")
+				aux_head = aux_head.sig
 
 	def eliminar(self, nodo:Nodo):
 		if nodo.ant != None:
@@ -93,30 +105,9 @@ class Usuario:
 
 #Creacion de Funciones
 
-#Compara dos listas enlazadas y devuelve una lista con los elementos en común
-def comparacion(lista1:ListaEnlazada, lista2:ListaEnlazada)->ListaEnlazada:
-    interseccion = ListaEnlazada()
-    aux = None
-    #se usara la lista con menos enlementos para recorrer
-    if lista1.cantidad < lista2.cantidad:
-        aux = lista1.head
-        while aux != None:
-            if lista2.buscar(aux.dato) != None:
-                interseccion.insertar(aux.dato)
-            aux = aux.sig
-    else:
-        aux = lista2.head
-        while aux != None:
-            if lista1.buscar(aux.dato) != None:
-                interseccion.insertar(aux.dato)
-            aux = aux.sig
-
-    return interseccion
-
-
 #Lectura y procesamiento de datos
 def lectura_datos(nombre_archivo)->tuple:
-    datos = pandas.read_csv(nombre_archivo, usecols=[0,2,6,8], nrows=10000)
+    datos = pandas.read_csv(nombre_archivo, usecols=[0,2,6,8], nrows=50000)
 
     usuarios = {}
     posts = {}
@@ -135,6 +126,35 @@ def lectura_datos(nombre_archivo)->tuple:
         usuarios[username].post_usuario.append(post)
 
     return usuarios, posts
+
+def creacion_amigos(usuarios:dict):
+
+    lista_usuarios = list(usuarios.values())
+
+    for usuario in usuarios.values():
+        cantidad_amigos = random.randint(0, 50)
+        amigos = random.sample(lista_usuarios, cantidad_amigos)
+
+        if usuario in amigos:
+            amigos.remove(usuario)
+
+        for amigo in amigos:
+            usuario.amigos.insertar(amigo)
+        #print(f"Estoy en creacion de amigos. EL tipo de amigos es: {type(usuario.amigos.head)}")
+
+def creacion_likes(usuarios:dict, posts:dict):
+    lista_usuarios = list(usuarios.keys())
+
+    for post in posts.values():
+        try:
+            usuarios_like = random.sample(lista_usuarios, abs(post.cant_likes))
+        except:
+            
+            post.cant_likes = len(lista_usuarios)
+            usuarios_like = random.sample(lista_usuarios, post.cant_likes)
+        for usuario in usuarios_like:
+            post.usuarios_likes.insertar(usuario)
+
 
 #Compara dos listas enlazadas y devuelve una lista con los elementos en común
 def comparacion(lista1:ListaEnlazada, lista2:ListaEnlazada)->ListaEnlazada:
@@ -161,11 +181,11 @@ def crear_indice_post(posts:dict)->dict:
     indice_post = {}
     for post in posts.values():
         for palabra in str(post.caption).lower().split(): 
-                palabra = palabra.strip(".?,#$!¿&[]}{/() ")
+                palabra = palabra.strip(".?,#$!¿&[]}{/()*+-:;\"='¿¡%|~` ")
                 if palabra not in stopwords:
                     if palabra not in indice_post:
                         indice_post[palabra] = ListaEnlazada()
-                    indice_post[palabra].insertar_sin_repeticion(post.caption)
+                    indice_post[palabra].insertar_sin_repeticion(post)
     return indice_post
 
 
@@ -174,10 +194,11 @@ def consulta_indice_post(indice_post: dict):
 
     consulta = input("Ingrese una palabra para buscar en los captions: ").lower().strip(".?,#$!¿&[]}{/() ")
     consulta = consulta.split()
+    consulta_sin_stopwords = []
     for palabra in consulta:
-        if palabra in stopwords:
-            consulta.remove(palabra)
-
+        if palabra not  in stopwords:
+            consulta_sin_stopwords.append(palabra)
+    consulta = consulta_sin_stopwords
     try:
         aux_list = indice_post[consulta[0]]
         for i in range(1,len(consulta)):
@@ -205,40 +226,14 @@ def consulta_indice_amigos(indice_amigos: dict):
     try:
         amigos = indice_amigos[consulta]
         print(f"Amigos de {consulta}: ")
+        print(f"amigos type:", type(amigos.head.dato))
         amigos.recorrer()
     except AttributeError:
         print(f"No se encontro un usuario con ese nombre {consulta}")
     except KeyError:
         print(f"No se encontró el usuario {consulta}")
 
-def creacion_amigos(usuarios:dict):
 
-    lista_usuarios = list(usuarios.keys())
-
-    for usuario in usuarios.values():
-        usuario.amigos = ListaEnlazada()
-        cantidad_amigos = random.randint(0, 50)
-        amigos = random.sample(lista_usuarios, cantidad_amigos)
-
-        if usuario in amigos:
-            amigos.remove(usuario)
-
-        for amigo in amigos:
-            usuario.amigos.insertar(amigo)
-
-def creacion_likes(usuarios:dict, posts:dict):
-    lista_usuarios = list(usuarios.keys())
-    for post in posts.values():
-        post.usuarios_likes = ListaEnlazada()
-        try:
-            usuarios_like = random.sample(lista_usuarios, abs(post.cant_likes))
-        except:
-            post.cant_likes = len(lista_usuarios)
-            usuarios_like = random.sample(lista_usuarios, post.cant_likes)
-            for usuario in usuarios_like:
-                post.usuarios_likes.insertar(usuario)
-
-    usuarios[lista_usuarios[0]].post_usuario[0].usuarios_likes.recorrer()
 
 
 stopwords = ["about", "above", "across", "after", "against", "along", "among", 
@@ -274,15 +269,32 @@ print("Fin creacion likes, inicio crear indice amigos --- %s seconds ---" % (tim
 indice_amigos = crear_indice_amigos(usuarios)
 print("Fin crear indice amigos --- %s seconds ---" % (time.time() - start_time))
 
-try:
-    print("Inicio consulta indice post --- %s seconds ---" % (time.time() - start_time))
-    consulta_indice_post(indice_post)
-    print("Fin consulta indice post, Inicio consulta indice amigos  --- %s seconds ---" % (time.time() - start_time))
-    consulta_indice_amigos(indice_amigos)
-    print("Fin consulta indice amigos --- %s seconds ---" % (time.time() - start_time))
-except AttributeError:
-    print("No se enocntraroh post con esos ternminos")
+#Grafico de las 10 palabras mas comunes en los captions
+lista = []
+for palabra in indice_post.keys():
+    lista.append((palabra, indice_post[palabra].cantidad))
 
+
+lista.sort(key=lambda x: x[1], reverse=True)
+
+plt.bar([x[0] for x in lista[:100]], [x[1] for x in lista[:100]])
+plt.xlabel("Palabras")
+plt.ylabel("Cantidad de posts")
+plt.title("Top 100 palabras más comunes en los captions")
+plt.xticks(rotation=45)
+plt.show()
+
+
+termino = ""
+while termino.lower().strip() !="s":
+    try:
+        
+        print("Inicio consulta indice post --- %s seconds ---" % (time.time() - start_time))
+        consulta_indice_post(indice_post)
+        print("Fin consulta indice post, Inicio consulta indice amigos  --- %s seconds ---" % (time.time() - start_time))
+        consulta_indice_amigos(indice_amigos)
+        print("Fin consulta indice amigos --- %s seconds ---" % (time.time() - start_time))
+    except AttributeError:
+        print("No se enocntraron post con esos ternminos")
+    termino = input("Desea finalizar la busqueda? Escriba s para terminar: ")
 print("Process final --- %s seconds ---" % (time.time() - start_time))
-
-
